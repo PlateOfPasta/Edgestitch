a4eda757ec8031bd4f759920e30106833a4517e5
PlateOfPasta <69741484+PlateOfPasta@users.noreply.github.com>
2020-09-10 21:59:39 -0500

    Add more utilities to PlayerUtil

--- edgestitch-util/src/main/kotlin/com/github/plateofpasta/edgestitch/player/PlayerUtil.kt
+++ edgestitch-util/src/main/kotlin/com/github/plateofpasta/edgestitch/player/PlayerUtil.kt
@@ -24,11 +24,25 @@
 
 package com.github.plateofpasta.edgestitch.player
 
+import com.github.plateofpasta.edgestitch.block.EdgestitchBlock
 import com.github.plateofpasta.edgestitch.world.EdgestitchLocation
+import io.netty.buffer.Unpooled
+import java.io.IOException
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket
 import net.minecraft.server.MinecraftServer
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.LiteralText
+import net.minecraft.text.MutableText
+import net.minecraft.text.Style
+import net.minecraft.util.Formatting
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
 
 /**
  * Creates a [ServerPlayerEntity] from a player name with all of the NBT player data loaded. Works
@@ -53,4 +67,68 @@ fun resolvePlayer(server: MinecraftServer, playerName: String): ServerPlayerEnti
   player = playerManager.createPlayer(profile) ?: return null
   val tag = playerManager.loadPlayerData(player) ?: return null
   return player
+}
+
+/**
+ * Sends the player a message.
+ *
+ * @param message [MutableText] to send the player.
+ */
+fun ServerPlayerEntity.sendMessage(message: MutableText) {
+  val style = Style.EMPTY.withColor(Formatting.YELLOW)
+  sendMessage(message.setStyle(style), false)
+}
+
+/**
+ * Sends the player a message. Delegates to the [MutableText] version.
+ *
+ * @param message String to send the player.
+ */
+fun ServerPlayerEntity.sendMessage(message: String) {
+  sendMessage(LiteralText(message))
+}
+
+/**
+ * Variadic version that delegates to the appropriate sendMessage for each message.
+ *
+ * @param messages Variadic argument of messages.
+ */
+fun ServerPlayerEntity.sendMessages(vararg messages: String) {
+  for (message in messages) {
+    sendMessage(message)
+  }
+}
+
+/**
+ * Variadic version that delegates to the appropriate sendMessage for each message.
+ *
+ * @param messages Variadic argument of messages.
+ */
+fun ServerPlayerEntity.endMessages(vararg messages: MutableText) {
+  for (message in messages) {
+    sendMessage(message)
+  }
+}
+
+/**
+ * Sends a fake block at the 3D coordinate to be visualized on the client.
+ *
+ * @param block What the fake block should look like.
+ * @param coord3D Position in the player's current world to change.
+ */
+@Environment(EnvType.SERVER)
+fun ServerPlayerEntity.sendFakeBlock(block: BlockState, coord3D: Vec3d?) {
+  // Construct the packet with a buffer so we can specific the block state at the location.
+  val packet = BlockUpdateS2CPacket()
+  val packetBuf = PacketByteBuf(Unpooled.buffer())
+  packetBuf.writeBlockPos(BlockPos(coord3D))
+  packetBuf.writeVarInt(Block.getRawIdFromState(block))
+  try {
+    // "Fill" the packet with our info.
+    packet.read(packetBuf)
+  } catch (e: IOException) {
+    return
+  }
+  // Fabric network API.
+  ServerSidePacketRegistry.INSTANCE.sendToPlayer(this, packet)
 }
